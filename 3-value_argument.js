5bd54fbff5cc99fd1faff8290c6fff046a797bce
theisraelolaleye <theisraelolaleye@gmail.com>
2025-09-12 17:27:46 +0100

    Value of my argument

--- 3-value_argument.js
+++ 3-value_argument.js
@@ -3,7 +3,7 @@
 const args = process.argv.slice(2);
 const first = args[0];
 
-if (count === 0) {
+if (first === undefined) {
   console.log("No argument");
 } else {
   console.log("Arguments found");
