624912e15d8f273a2d429f9cd8c10c6bd73c09f7
theisraelolaleye <theisraelolaleye@gmail.com>
2025-09-14 16:04:07 +0100

    8. Square

---
+++ 8-square.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const size = Number(process.argv[2]);
+
+if (isNaN(size)) {
+  console.log('Missing size');
+} else {
+  const line = 'X'.repeat(size);
+  for (let i = 0; i < size; i++) {
+    console.log(line);
+  }
+}
